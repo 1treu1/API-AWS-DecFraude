396da1f565c9b4181eb37b7b2d2400c9d14788d5
Medina Jimenez Luis Herney <lhmedina@fanalca.com>
2024-01-07 19:13:46 -0500

    Haciendo que los campos no sean abligatorios

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ loaded_model = load_model('/home/ubuntu/API-AWS-DecFraude/Model_Prod_P6/model_pr
 app = FastAPI()
 @app.post("/convert_to_dataframe/")
 async def convert_to_dataframe(
-    INGRESOS: int = Query(..., description="Ingrese el valor de INGRESOS", example=11000000),
-    EGRESOS: int = Query(..., description="Ingrese el valor de EGRESOS", example=4500000),
-    Dist_Mean_NAL: float = Query(..., description="Ingrese el valor de Dist_Mean_NAL", example=614.039978),
-    Dist_sum_NAL: float = Query(..., description="Ingrese el valor de Dist_sum_NAL", example=1228.069946),
-    FECHA_VIN: int = Query(..., description="Ingrese el valor de FECHA_VIN ", example=19890814),
-    FECHA: int = Query(..., description="Ingrese el valor de FECHA ", example=20150506),
-    OFICINA_VIN: int = Query(..., description="Ingrese el valor de OFICINA_VIN", example=961),
-    VALOR: float = Query(..., description="Ingrese el valor de VALOR", example=143202.656250),
-    HORA_AUX: int = Query(..., description="Ingrese el valor de HORA_AUX ", example=20),
-    EDAD: int = Query(..., description="Ingrese el valor de EDAD ", example=56)
+    INGRESOS: int = Query(None, description="Ingrese el valor de INGRESOS", example=11000000),
+    EGRESOS: int = Query(None, description="Ingrese el valor de EGRESOS", example=4500000),
+    Dist_Mean_NAL: float = Query(None, description="Ingrese el valor de Dist_Mean_NAL", example=614.039978),
+    Dist_sum_NAL: float = Query(None, description="Ingrese el valor de Dist_sum_NAL", example=1228.069946),
+    FECHA_VIN: int = Query(None, description="Ingrese el valor de FECHA_VIN ", example=19890814),
+    FECHA: int = Query(None, description="Ingrese el valor de FECHA ", example=20150506),
+    OFICINA_VIN: int = Query(None, description="Ingrese el valor de OFICINA_VIN", example=961),
+    VALOR: float = Query(None, description="Ingrese el valor de VALOR", example=143202.656250),
+    HORA_AUX: int = Query(None, description="Ingrese el valor de HORA_AUX ", example=20),
+    EDAD: int = Query(None, description="Ingrese el valor de EDAD ", example=56)
 ):
     data = {
         'INGRESOS': INGRESOS,
